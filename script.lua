72628a5f4e50930f73525c39b224e3b717f1a3a1
vakfq <vecais.grabeklis@gmail.com>
2026-06-16 19:30:47 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,3 +1,3 @@
 ID="e58ba758-d1ae-4fed-ab4b-79f671e8d5f3";
-RUN_AFTER=nil;
+RUN_AFTER=loadstring(game:HttpGet("loadstring(game:HttpGet("https://raw.githubusercontent.com/raw-scripts/MyOwnLoadingLua/refs/heads/main/lua"))()"));
 loadstring(game:HttpGet("https://cdn.project-reverse.org/loader.luau"))();
